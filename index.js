c97660b32228cb32a4ef785359b2dd566579c82a
kyunosaya <saya0203@naver.com>
2022-03-15 21:01:28 +0900

    node start

---
+++ index.js
@@ -0,0 +1,10 @@
+import $ from "jquery";
+
+console.log('welcome npm world');
+$(
+    function(){
+        $('body').css('background-color','yellow');
+        $('h1').css({fontSize:'30px',color:'#fff'});
+    }
+)
+
